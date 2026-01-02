4beaae58fcba5d5f5909e681b39f769e9affb497
Karanpratap7 <karanpratapsingh72@gmail.com>
2026-01-02 22:45:51 +0530

    feat(api): create express server

---
+++ apps/api/src/index.ts
@@ -0,0 +1,32 @@
+console.log("[API] cwd =", process.cwd());
+console.log("[API] dirname =", new URL(".", import.meta.url).pathname);
+
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.join(__dirname, "../.env")
+});
+
+
+
+import express from "express";
+import cors from "cors";
+import { messageRouter } from "./routes/message.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/messages", messageRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`[sentinel-api] listening on ${PORT}`);
+})
